1c72185f2374c6d759704290eae665aff1d33a7b
DadaKobeleva <kobelevadara@yandex.ru>
2025-12-15 15:56:56 +0500

    Созданный классы для приложения. Винокуров

--- Classes/Events.cs
+++ Classes/Events.cs
@@ -8,6 +8,12 @@ namespace TaskManagerTelegramBot_Vinokurov.Classes
 {
     public class Events
     {
-      
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+        public Events(DateTime time, string message)
+        {
+            Time = time;
+            Message = message;
+        }
     }
 }
--- Classes/Users.cs
+++ Classes/Users.cs
@@ -8,6 +8,13 @@ namespace TaskManagerTelegramBot_Vinokurov.Classes
 {
     public class Users
     {
-       
+        public long IdUser { get; set; }
+        public List<Events> Events { get; set; }
+        public Users(long idUser)
+        {
+            IdUser = idUser;
+            Events = new List<Events>();
+        }
+
     }
 }
